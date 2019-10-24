d07fad6ce9024f82236062b9ef87c2edbfd3a4f7
Gao <Jiayi.Gao@emc.com>
2019-10-24 17:55:28 +0800

    vuex

--- examples/counter/Counter.vue
+++ examples/counter/Counter.vue
@@ -5,16 +5,21 @@
     <button @click="decrement">-</button>
     <button @click="incrementIfOdd">Increment if odd</button>
     <button @click="incrementAsync">Increment async</button>
+    <p>{{ value1 }}</p>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
-
+import { mapState } from 'vuex'
 export default {
   computed: mapGetters([
     'evenOrOdd'
   ]),
+   // mapState 辅助函数,帮助我们生产计算属性
+  computed: mapState({
+    value1: state => state.count + 'gaojy',
+  }),
   methods: mapActions([
     'increment',
     'decrement',
--- examples/counter/app.js
+++ examples/counter/app.js
@@ -3,8 +3,11 @@ import Vue from 'vue'
 import Counter from './Counter.vue'
 import store from './store'
 
+
 new Vue({
   el: '#app',
+  // 把store对象提供给“store”选项，这可以把store的实例注入所有的子组件
   store,
+
   render: h => h(Counter)
 })
